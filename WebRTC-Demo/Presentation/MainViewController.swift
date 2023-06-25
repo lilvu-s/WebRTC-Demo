206bd8608b1770346456260e468147f68e397a59
Angelina <anhelina.semenchenko@gmail.com>
2023-06-26 02:38:09 +0300

    Setup base layers

---
+++ WebRTC-Demo/Presentation/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  WebRTC-Demo
+//
+//  Created by Ангеліна Семенченко on 25.06.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
